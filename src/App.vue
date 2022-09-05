c3b6cf584e9705ba93c98a3aa1ef30b3b8750703
Wesley Erasmus <88334921+WesleyErasmus@users.noreply.github.com>
2022-09-05 10:18:46 +0200

    Created Vue Project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+    <div class="wrapper">
+
+      
+    </div>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home-page-wrapper">
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
