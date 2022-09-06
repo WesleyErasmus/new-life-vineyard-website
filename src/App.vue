100057aab8eb9ec4c6545e3e13867b193d786dee
Wesley Erasmus <88334921+WesleyErasmus@users.noreply.github.com>
2022-09-06 13:03:36 +0200

    Building home page hero image

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,83 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-
-</script>
-
 <template>
-    <div class="wrapper">
+  <div class="wrapper">
+    <nav id="app-navbar">
+      <div class="church-logo-container">
+        <img class="nlv-logo" src="./assets/nlv-logo-1.jpg" alt="NLV Logo">
+      </div>
+      <div class="nav-links">
+        <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+        <RouterLink :to="{ name: 'home' }">Our mission</RouterLink>
+        <RouterLink :to="{ name: 'home' }">Prayer</RouterLink>
+        <RouterLink :to="{ name: 'home' }">Giving</RouterLink>
+        <RouterLink :to="{ name: 'home' }">Become a member</RouterLink>
+        <RouterLink :to="{ name: 'home' }">Contact</RouterLink>
+      </div>
+    </nav>
 
-      
-    </div>
+
+  </div> <!-- End of app wrapper -->
   <RouterView />
 </template>
 
-<style scoped>
+<script>
+import { RouterLink, RouterView } from 'vue-router';
 
+export default {
+
+}
+</script>
+
+<style scoped>
+#app-navbar {
+  padding: 1.65rem;
+  display: flex;
+  justify-content: center;
+  background: var(--secondary-color);
+}
+
+.church-logo-container {
+  position: absolute;
+  top: 0;
+  left: 4%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+
+.nav-links {
+  height: 100%;
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+}
+
+.nav-links a {
+  height: 100%;
+  margin: 10px 0 10px 20px;
+
+}
+
+.nlv-logo {
+  width: 75px;
+  border-radius: 50%;
+}
+
+.nav-links {
+  font-size: 20px;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: var(--primary-color);
+  transition: 0.4s;
+}
+
+
+@media (hover: hover) {
+  a:hover {
+    color: #ddd;
+    text-decoration: underline 3px solid var(--primary-color);
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+// Bootstrap import
+import "bootstrap/dist/css/bootstrap.css";
 
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import "./assets/main.css";
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+
+app.mount("#app");
+
+// Bootstrap popper.js
+import 'bootstrap/dist/js/bootstrap.js'
+import { popper } from "@popperjs/core";
